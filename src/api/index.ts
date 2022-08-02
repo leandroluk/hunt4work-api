5e174e2e4b0c5382bfd233843c3a43589a0d7487
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 22:37:04 -0300

    adding middlewares and routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import * as middlewares from './middlewares';
+import routes from './routes';
 
 const api = express();
+api.use(middlewares.bodyParser);
+api.use(middlewares.cors);
+api.use(middlewares.logger);
+api.use(middlewares.security);
+api.use(routes);
+api.use(middlewares.errorHandler);
 
 export default api;
