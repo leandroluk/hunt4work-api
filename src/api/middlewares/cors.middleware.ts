84eb0b3626c5e6467ae1b67ab5f474ea14a43aee
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 22:35:21 -0300

    refactor: export named cors middleware

--- src/api/middlewares/cors.middleware.ts
+++ src/api/middlewares/cors.middleware.ts
@@ -1,9 +1,13 @@
 import vars from '$/vars';
 import { NextFunction, Request, Response } from 'express';
 
-export default (_req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-control-allow-origin', vars.api.corsOrigin);
-  res.set('access-control-allow-methods', '*');
-  res.set('access-control-allow-headers', '*');
+export const cors = (
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  res.setHeader('access-control-allow-origin', vars.api.corsOrigin);
+  res.setHeader('access-control-allow-methods', '*');
+  res.setHeader('access-control-allow-headers', '*');
   next();
 };
