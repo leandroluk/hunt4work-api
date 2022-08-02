fe06953782d235c4f3b98fdee025e2a846555f43
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 22:05:09 -0300

    feat: create base vars file

---
+++ src/vars.ts
@@ -0,0 +1,14 @@
+import packageJson from '../package.json';
+const { cwd, env } = process;
+
+export default {
+  api: {
+    name: packageJson.name,
+    path: cwd(),
+    port: Number(env.api_port ?? 3000),
+    corsOrigin: env.api_corsOrigin ?? '*',
+  },
+  mongo: {
+    url: env.mongo_url ?? 'mongodb://localhost:27017/db',
+  },
+};
