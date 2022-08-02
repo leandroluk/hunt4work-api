2780fd1171051ed3715a64e8986e694a36ef84b4
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 22:08:59 -0300

    refactor: adjust vars import

--- src/api/middlewares/cors.middleware.ts
+++ src/api/middlewares/cors.middleware.ts
@@ -1,5 +1,5 @@
+import vars from '$/vars';
 import { NextFunction, Request, Response } from 'express';
-import vars from '../../vars';
 
 export default (_req: Request, res: Response, next: NextFunction): void => {
   res.set('access-control-allow-origin', vars.api.corsOrigin);
