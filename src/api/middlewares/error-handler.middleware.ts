77214e54ef5adb1bc0c236631e3f0b90bde850ac
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 21:58:48 -0300

    create base error handler

---
+++ src/api/middlewares/error-handler.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+
+const errors: Record<string, number> = {
+  ValidationError: 400,
+  NotFoundError: 404,
+  ConflitError: 409,
+};
+
+export const errorHandlerMiddleware = (
+  { name, message }: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const status = errors[name];
+  if (!status) {
+    return res.sendStatus(500);
+  }
+  res.status(status).json({ message });
+};
