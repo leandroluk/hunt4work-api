d0cdde7e0e98c243bc158e26e6810929affebbfe
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-08-01 22:36:27 -0300

    feat: adding barrel in middlewares

---
+++ src/api/middlewares/index.ts
@@ -0,0 +1,6 @@
+export * from './body-parser.middleware';
+export * from './cors.middleware';
+export * from './error-handler.middleware';
+export * from './logger.middleware';
+export * from './security.middleware';
+
